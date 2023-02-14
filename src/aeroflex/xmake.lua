d5e889712cfb5c8e0419b3533a11842eb8a7477c
SamAyala <72941105+samayala22@users.noreply.github.com>
2023-02-14 11:10:20 -0500

    add xmake run args

--- src/aeroflex/xmake.lua
+++ src/aeroflex/xmake.lua
@@ -1,5 +1,6 @@
 target("aeroflex")
     set_kind("binary")
+    set_runargs({"-f"}, {"../../../../examples/rans/example.rans"}, {"-o"}, {"../../../../examples/rans/test.vtu"})
     add_deps("flexgui", "implot", "rans")
     add_packages("imgui", "eigen", "openmp")
     add_files("src/*.cpp")
