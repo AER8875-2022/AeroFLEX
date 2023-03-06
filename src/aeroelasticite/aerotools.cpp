f2a9e51ee8e6d2e532e0b0e142b2913b54315da5
amnia02 <114753345+amnia02@users.noreply.github.com>
2023-03-06 14:26:12 -0500

    Update aerotools.cpp

--- src/aeroelasticite/aerotools.cpp
+++ src/aeroelasticite/aerotools.cpp
@@ -64,7 +64,105 @@ namespace aero{
         }
     }
     
-    auto LoadInterpol() {
+auto LoadInterpol(){
+    
+    // Loading VLM forceacting Point
+    vector<vector <double>> point_fa;
+    int n=size.wings(); // nombre de wingstations=size (wings)
+    int m=size.wingstation; // nombre de vortexRings sur une wingstation
+    int i=0 // numéro du premier vortexring d une wingstation
+    
+    for (int j=0; j<=n; ++j){
+        auto wingstation = wingStations[wings[0].get_wingStationsIDs()[j]];
+        m=sizeowingstation);
+        
+        auto vortexring= vortexRings[wstation.get_vortexIDs()[i]];
+        point_fa.push.back(vortexRing[i].ForcesActingPoint(nodes, vortexRings));
+        
+        i= i + m;
+        }
+    
+    //    Loading STRUCTURE connectivity
+    vector<vector <double>> point_fs;
+    
+    
+
+    // Projeter le point_fa sur la droite et calcul de la distance
+    vector<vector<double>> point_fp [n];
+    vector <double> dist[n];
+    vector <double> v[3]
+    
+    vector <double> u(3); // vecteur directeur de la poutre
+    u[1]= point_fs[2][1] - point_fs[1][1];
+    u[2]= point_fs[2][2] - point_fs[1][2];
+    u[3]= point_fs[2][3] - point_fs[1][3];
+    
+    for (int i=0; i<=n; ++i){
+        double t;
+        t= (u[1](point_fa[i][1]-point_fs[1][1]) + u[2](point_fa[i][2]-point_fs[1][2]) + u[1](point_fa[i][1]-point_fs[1][1]))/norme(u);
+        
+        point_fp[i][1]=point_fs[1][1] + t*u[1];
+        point_fp[i][2]=point_fs[1][2] + t*u[2];
+        point_fp[i][3]=point_fs[1][3] + t*u[3];
+        
+        v[1]= point_fa[i][1] - point_fp[i][1];
+        v[2]= point_fa[i][2] - point_fp[i][2];
+        v[3]= point_fa[i][3] - point_fp[i][3];
+        dist[i]=norme(v);
+        
+     }
+    
+    // Calcul des coefficients d'interpolation
+    vector <vector> forces_s[m][6];
+    double dist_0;
+    double dist_1;
+    double dist_2;
+    
+    for (int i=0; i<n; ++i){
+        auto forces = model.forces_to_inertial(i);
+
+        
+        for (int j=0; j<m; ++j){
+            v[1]= point_fs[j][1] - point_fs[j+1][1];
+            v[2]= point_fs[j][2] - point_fp[j+1][2];
+            v[3]= point_fs[j][3] - point_fp[j+1][3];
+            dist_0=norme(v);
+            
+            v[1]= point_fs[j][1] - point_fp[i][1];
+            v[2]= point_fs[j][2] - point_fp[i][2];
+            v[3]= point_fs[j][3] - point_fp[i][3];
+            dist_1=norme(v);
+            
+            v[1]= point_fs[j+1][1] - point_fp[i][1];
+            v[2]= point_fs[j+1][2] - point_fp[i][2];
+            v[3]= point_fs[j+1][3] - point_fp[i][3];
+            dist_2=norme(v);
+            
+            if dist_1<=dist_0{
+                epsilon_l= dist_2/dist_0;
+                epsilon_r= dist_1/dist_0;
+                
+                forces_s[j][1]= forces_s[j][1] + epsilon_l*forces[i][1];
+                forces_s[j][2]= forces_s[j][2] + epsilon_l*forces[i][2];
+                forces_s[j][3]= forces_s[j][3] + epsilon_l*forces[i][3];
+                forces_s[j][4]= forces_s[j][4] + epsilon_l*forces[i][4]*dist[i];
+                forces_s[j][5]= forces_s[j][5] + epsilon_l*forces[i][5]*dist[i];
+                forces_s[j][6]= forces_s[j][6] + epsilon_l*forces[i][6]*dist[i];
+                
+                forces_s[j+1][1]= forces_s[j+1][1] + epsilon_l*forces[i][1];
+                forces_s[j+1][2]= forces_s[j+1][2] + epsilon_l*forces[i][2];
+                forces_s[j+1][3]= forces_s[j+1][3] + epsilon_l*forces[i][3];
+                forces_s[j+1][4]= forces_s[j+1][4] + epsilon_l*forces[i][4]*dist[i];
+                forces_s[j+1][5]= forces_s[j+1][5] + epsilon_l*forces[i][5]*dist[i];
+                forces_s[j+1][6]= forces_s[j+1][6] + epsilon_l*forces[i][6]*dist[i];
+                
+            }
+            
+        }
+     
+    }
+      
+}
         double dotProduct(const vector<double>& a, const vector<double>& b) {
             if (a.size() != b.size()) {
                 throw std::runtime_error("Vectors must have the same dimension.");
@@ -92,6 +190,3 @@ namespace aero{
              }
              return sqrt(sum);
          }
-
-    return 0;
-}
