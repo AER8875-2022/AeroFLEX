98604aceddf6328ebbfe2d6d25265f9593a78f67
Samuel Ayala <samayala22@gmail.com>
2023-03-13 01:31:28 -0400

    [GUI] added file dialog example

--- src/aeroflex/src/app.cpp
+++ src/aeroflex/src/app.cpp
@@ -1,18 +1,20 @@
-#include "Application.h"
 #include "EntryPoint.h"
+
 #include "imgui.h"
 #include "implot.h"
 #include "tinyconfig.hpp"
 
-#include "common_aeroflex.hpp"
+#include "Application.h"
+#include "FileDialog.hpp"
 
-#include <rans/rans.h>
-#include <vlm/vlm.hpp>
-
+#include <string>
 #include <future>
 #include <thread>
-#include <optional>
-#include <unordered_map>
+#include <ctime>
+
+#include "common_aeroflex.hpp"
+#include <rans/rans.h>
+#include <vlm/vlm.hpp>
 
 template <class T>
 bool is_future_done(std::future<T> const& f) {
@@ -56,127 +58,28 @@ class Aero {
 		Aero(rans::Rans &rans, vlm::VLM &vlm, GUIHandler &gui);
 };
 
-struct ExampleAppLog
-{
-    ImGuiTextBuffer     Buf;
-    ImGuiTextFilter     Filter;
-    ImVector<int>       LineOffsets; // Index to lines offset. We maintain this with AddLog() calls.
-    bool                AutoScroll;  // Keep scrolling if already at the bottom.
+struct RansLayer : public FlexGUI::Layer {
+	virtual void OnUIRender() override;
+	Aero &aero;
+	RansLayer(Aero &aero) : aero(aero) {};
+};
 
-    ExampleAppLog()
-    {
-        AutoScroll = true;
-        Clear();
-    }
+struct ButtonLayer : public FlexGUI::Layer {
+	virtual void OnUIRender() override;
+	Aero &aero;
+	ButtonLayer(Aero &aero) : aero(aero) {};
+};
 
-    void    Clear()
-    {
-        Buf.clear();
-        LineOffsets.clear();
-        LineOffsets.push_back(0);
-    }
+struct GraphLayer : public FlexGUI::Layer {
+	virtual void OnUIRender() override;
+	Aero &aero;
+	GraphLayer(Aero &aero) : aero(aero) {};
+};
 
-    void    AddLog(const char* fmt, ...) IM_FMTARGS(2)
-    {
-        int old_size = Buf.size();
-        va_list args;
-        va_start(args, fmt);
-        Buf.appendfv(fmt, args);
-        va_end(args);
-        for (int new_size = Buf.size(); old_size < new_size; old_size++)
-            if (Buf[old_size] == '\n')
-                LineOffsets.push_back(old_size + 1);
-    }
-
-    void    Draw(const char* title, bool* p_open = NULL)
-    {
-        if (!ImGui::Begin(title, p_open))
-        {
-            ImGui::End();
-            return;
-        }
-
-        // Options menu
-        if (ImGui::BeginPopup("Options"))
-        {
-            ImGui::Checkbox("Auto-scroll", &AutoScroll);
-            ImGui::EndPopup();
-        }
-
-        // Main window
-        if (ImGui::Button("Options"))
-            ImGui::OpenPopup("Options");
-        ImGui::SameLine();
-        bool clear = ImGui::Button("Clear");
-        ImGui::SameLine();
-        bool copy = ImGui::Button("Copy");
-        ImGui::SameLine();
-        Filter.Draw("Filter", -100.0f);
-
-        ImGui::Separator();
-
-        if (ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar))
-        {
-            if (clear)
-                Clear();
-            if (copy)
-                ImGui::LogToClipboard();
-
-            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
-            const char* buf = Buf.begin();
-            const char* buf_end = Buf.end();
-            if (Filter.IsActive())
-            {
-                // In this example we don't use the clipper when Filter is enabled.
-                // This is because we don't have random access to the result of our filter.
-                // A real application processing logs with ten of thousands of entries may want to store the result of
-                // search/filter.. especially if the filtering function is not trivial (e.g. reg-exp).
-                for (int line_no = 0; line_no < LineOffsets.Size; line_no++)
-                {
-                    const char* line_start = buf + LineOffsets[line_no];
-                    const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
-                    if (Filter.PassFilter(line_start, line_end))
-                        ImGui::TextUnformatted(line_start, line_end);
-                }
-            }
-            else
-            {
-                // The simplest and easy way to display the entire buffer:
-                //   ImGui::TextUnformatted(buf_begin, buf_end);
-                // And it'll just work. TextUnformatted() has specialization for large blob of text and will fast-forward
-                // to skip non-visible lines. Here we instead demonstrate using the clipper to only process lines that are
-                // within the visible area.
-                // If you have tens of thousands of items and their processing cost is non-negligible, coarse clipping them
-                // on your side is recommended. Using ImGuiListClipper requires
-                // - A) random access into your data
-                // - B) items all being the  same height,
-                // both of which we can handle since we have an array pointing to the beginning of each line of text.
-                // When using the filter (in the block of code above) we don't have random access into the data to display
-                // anymore, which is why we don't use the clipper. Storing or skimming through the search result would make
-                // it possible (and would be recommended if you want to search through tens of thousands of entries).
-                ImGuiListClipper clipper;
-                clipper.Begin(LineOffsets.Size);
-                while (clipper.Step())
-                {
-                    for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++)
-                    {
-                        const char* line_start = buf + LineOffsets[line_no];
-                        const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
-                        ImGui::TextUnformatted(line_start, line_end);
-                    }
-                }
-                clipper.End();
-            }
-            ImGui::PopStyleVar();
-
-            // Keep up at the bottom of the scroll region if we were already at the bottom at the beginning of the frame.
-            // Using a scrollbar or mouse-wheel will take away from the bottom edge.
-            if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
-                ImGui::SetScrollHereY(1.0f);
-        }
-        ImGui::EndChild();
-        ImGui::End();
-    }
+struct ConsoleLayer : public FlexGUI::Layer {
+	virtual void OnUIRender() override;
+	Aero &aero;
+	ConsoleLayer(Aero &aero) : aero(aero) {};
 };
 
 void solve(rans::Rans &rans) {
@@ -330,30 +233,6 @@ void Aero::config_save_await() {
 	signal_status_busy = false;
 }
 
-struct RansLayer : public FlexGUI::Layer {
-	virtual void OnUIRender() override;
-	Aero &aero;
-	RansLayer(Aero &aero) : aero(aero) {};
-};
-
-struct ButtonLayer : public FlexGUI::Layer {
-	virtual void OnUIRender() override;
-	Aero &aero;
-	ButtonLayer(Aero &aero) : aero(aero) {};
-};
-
-struct GraphLayer : public FlexGUI::Layer {
-	virtual void OnUIRender() override;
-	Aero &aero;
-	GraphLayer(Aero &aero) : aero(aero) {};
-};
-
-struct ConsoleLayer : public FlexGUI::Layer {
-	virtual void OnUIRender() override;
-	Aero &aero;
-	ConsoleLayer(Aero &aero) : aero(aero) {};
-};
-
 void RansLayer::OnUIRender() {
 	ImGui::Begin("RANS");
 
@@ -426,6 +305,22 @@ void ButtonLayer::OnUIRender() {
 			aero.config_save_await();
 		};
 
+		static char* file_dialog_buffer = nullptr;
+		static char path[500] = "";
+		static FlexGUI::FileDialog fd;
+
+		ImGui::TextUnformatted("Path: ");
+		ImGui::InputText("##path", path, sizeof(path));
+		ImGui::SameLine();
+		
+		if (ImGui::Button("Browse##path")) {
+			file_dialog_buffer = path;
+			fd.file_dialog_open = true;
+			fd.type = FlexGUI::FileDialogType::OpenFile;
+		}
+		
+		fd.Show(file_dialog_buffer, sizeof(file_dialog_buffer));
+
 		ImGui::End();
 
 		// ImGui::ShowDemoWindow();
@@ -457,42 +352,106 @@ void GraphLayer::OnUIRender() {
 	}
 };
 
+class ConsoleLog
+{
+	public:
+		void log(const char* msg) {
+			time_t ttime = time(0);
+			tm *t = localtime(&ttime);
+			add_entry("[%02d:%02d:%02d] : %s\n", t->tm_hour, t->tm_min, t->tm_sec, msg);
+		}
+
+		void draw(const char* title, bool* p_open = NULL) {
+			if (!ImGui::Begin(title, p_open)) {
+				ImGui::End();
+				return;
+			}
+
+			// Main window
+			bool clear = ImGui::Button("Clear");
+			ImGui::SameLine();
+			bool copy = ImGui::Button("Copy");
+			ImGui::SameLine();
+			Filter.Draw("Filter", -100.0f);
+
+			ImGui::Separator();
+
+			if (ImGui::BeginChild("scrolling", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar))
+			{
+				if (clear)
+					clear_console();
+				if (copy)
+					ImGui::LogToClipboard();
+
+				ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+				const char* buf = Buf.begin();
+				const char* buf_end = Buf.end();
+				if (Filter.IsActive()) {
+					for (int line_no = 0; line_no < LineOffsets.Size; line_no++)
+					{
+						const char* line_start = buf + LineOffsets[line_no];
+						const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+						if (Filter.PassFilter(line_start, line_end))
+							ImGui::TextUnformatted(line_start, line_end);
+					}
+				} else {
+					ImGuiListClipper clipper;
+					clipper.Begin(LineOffsets.Size);
+					while (clipper.Step()) {
+						for (int line_no = clipper.DisplayStart; line_no < clipper.DisplayEnd; line_no++) {
+							const char* line_start = buf + LineOffsets[line_no];
+							const char* line_end = (line_no + 1 < LineOffsets.Size) ? (buf + LineOffsets[line_no + 1] - 1) : buf_end;
+							ImGui::TextUnformatted(line_start, line_end);
+						}
+					}
+					clipper.End();
+				}
+				ImGui::PopStyleVar();
+
+				if (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+					ImGui::SetScrollHereY(1.0f);
+			}
+			ImGui::EndChild();
+			ImGui::End();
+		}
+
+		void add_entry(const char* fmt, ...) IM_FMTARGS(2)
+		{
+			int old_size = Buf.size();
+			va_list args;
+			va_start(args, fmt);
+			Buf.appendfv(fmt, args);
+			va_end(args);
+			for (int new_size = Buf.size(); old_size < new_size; old_size++)
+				if (Buf[old_size] == '\n')
+					LineOffsets.push_back(old_size + 1);
+		};
+
+	private:
+		ImGuiTextBuffer Buf;
+		ImGuiTextFilter Filter;
+		ImVector<int> LineOffsets; // Index to lines offset. We maintain this with AddLog() calls.
+    	const bool AutoScroll = true;  // Keep scrolling if already at the bottom.
+
+		void clear_console() {
+			Buf.clear();
+			LineOffsets.clear();
+			LineOffsets.push_back(0);
+		};
+};
+
 void ConsoleLayer::OnUIRender() {
-    static ExampleAppLog log;
+    static ConsoleLog log;
 
     ImGui::Begin("Console", NULL);
     std::optional<std::string> txt = aero.gui.msg.pop();
 
     if (txt.has_value()) {
-        log.AddLog(txt.value().c_str());
+        log.log(txt.value().c_str());
 		std::cout << "should print" << std::endl;
     }
     ImGui::End();
-    log.Draw("Console", NULL);
-
-    // For the demo: add a debug button _BEFORE_ the normal log window contents
-    // We take advantage of a rarely used feature: multiple calls to Begin()/End() are appending to the _same_ window.
-    // Most of the contents of the window will be added by the log.Draw() call.
-    // ImGui::SetNextWindowSize(ImVec2(500, 400), ImGuiCond_FirstUseEver);
-    // ImGui::Begin("Example: Log", p_open);
-    // if (ImGui::SmallButton("[Debug] Add 5 entries"))
-    // {
-    //     static int counter = 0;
-    //     const char* categories[3] = { "info", "warn", "error" };
-    //     const char* words[] = { "Bumfuzzled", "Cattywampus", "Snickersnee", "Abibliophobia", "Absquatulate", "Nincompoop", "Pauciloquent" };
-    //     for (int n = 0; n < 5; n++)
-    //     {
-    //         const char* category = categories[counter % IM_ARRAYSIZE(categories)];
-    //         const char* word = words[counter % IM_ARRAYSIZE(words)];
-    //         log.AddLog("[%05d] [%s] Hello, current time is %.1f, here's a word: '%s'\n",
-    //             ImGui::GetFrameCount(), category, ImGui::GetTime(), word);
-    //         counter++;
-    //     }
-    // }
-    // ImGui::End();
-
-    // // Actually call in the regular Log helper (which will Begin() into the same window as we just did)
-    // log.Draw("Example: Log", p_open);
+    log.draw("Console", NULL);
 }
 
 FlexGUI::Application* CreateApplication(int argc, char** argv, Aero& aero)
