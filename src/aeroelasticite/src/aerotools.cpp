6a404bc56028c23f9ca5b79f9f8261506edfe4c9
amnia02 <amadou.niane@polymtl.ca>
2023-03-14 11:13:41 -0400

    Update aerotools.cpp

--- src/aeroelasticite/src/aerotools.cpp
+++ src/aeroelasticite/src/aerotools.cpp
@@ -21,10 +21,12 @@ using namespace structure;
 
 namespace aero{
     struct interpolation;
+    struct interpolation_f;
 
 
     void DispInterpol( interpolation &pos,std::vector<surface::wingStation> wingStations,
-                       std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,std::vector<Vector3d> nodes,map<int,Vector3d> mapStruct,map<int, int> mapStructni) {
+                       std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,
+                      std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,std::vector<Vector3d> nodes,map<int,Vector3d> mapStruct,map<int, int> mapStructni) {
         for (k = 0; k <wings.size() ; ++k)
         {
             for (i = 0; i <wingStations.size() ; ++i)
@@ -127,116 +129,174 @@ namespace aero{
         }
         return node ;
     }
-   auto LoadInterpol(){
-    
-    // Loading VLM forceacting Point
-    vector<vector <double>> point_fa;
-    int n=size.wings(); // nombre de wingstations=size (wings)
-    int m=size.wingstation; // nombre de vortexRings sur une wingstation
-    int i=0 // numéro du premier vortexring d une wingstation
-    
-    for (int j=0; j<=n; ++j){
-        auto wingstation = wingStations[wings[0].get_wingStationsIDs()[j]];
-        m=sizeowingstation);
+   void LoadInterpol(interpolation_f &force, std::vector<surface::wingStation> wingStations,
+                       std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,map<int,Vector3d> mapStruct,map<int, int> mapStructni)
+   {
+        // Loading VLM forceacting Point
         
-        auto vortexring= vortexRings[wstation.get_vortexIDs()[i]];
-        point_fa.push.back(vortexRing[i].ForcesActingPoint(nodes, vortexRings));
+        int n=wings.size(); // nombre de wingstations=size (wings)
+        int m=wingstation.size(); // nombre de vortexRings sur une wingstation
+        int i=0 // numéro du premier vortexring d une wingstation
         
-        i= i + m;
+        for (int j=0; j<=n; ++j){
+            auto wingstation = wingStations[wings[0].get_wingStationsIDs()[j]];
+            m=sizeowingstation);
+            
+            auto vortexring= vortexRings[wstation.get_vortexIDs()[i]];
+            force.point_fa.push.back(vortexRing[i].ForcesActingPoint(nodes, vortexRings));
+            
+            i= i + m;
         }
-    
-    //    Loading STRUCTURE connectivity
-    vector<vector <double>> point_fs;
-    
-    
+        
+        //    Loading STRUCTURE connectivity
 
-    // Projeter le point_fa sur la droite et calcul de la distance
-    vector<vector<double>> point_fp [n];
-    vector <double> dist[n];
-    vector <double> v[3]
-    
-    vector <double> u(3); // vecteur directeur de la poutre
-    u[1]= point_fs[2][1] - point_fs[1][1];
-    u[2]= point_fs[2][2] - point_fs[1][2];
-    u[3]= point_fs[2][3] - point_fs[1][3];
-    
-    for (int i=0; i<=n; ++i){
-        double t;
-        t= (u[1](point_fa[i][1]-point_fs[1][1]) + u[2](point_fa[i][2]-point_fs[1][2]) + u[1](point_fa[i][1]-point_fs[1][1]))/norme(u);
-        
-        point_fp[i][1]=point_fs[1][1] + t*u[1];
-        point_fp[i][2]=point_fs[1][2] + t*u[2];
-        point_fp[i][3]=point_fs[1][3] + t*u[3];
-        
-        v[1]= point_fa[i][1] - point_fp[i][1];
-        v[2]= point_fa[i][2] - point_fp[i][2];
-        v[3]= point_fa[i][3] - point_fp[i][3];
-        dist[i]=norme(v);
-        
-     }
-    
-    // Calcul des coefficients d'interpolation
-    vector <vector> forces_s[m][6];
-    double dist_0;
-    double dist_1;
-    double dist_2;
-    
-    for (int i=0; i<n; ++i){
-        auto forces = model.forces_to_inertial(i);
+        for (int k=0, k<map.size(), ++k)
+        {
+            force.point_fs.push.back(vector<double>(3));
 
+        }
+
+
+        for (auto s : mapStructni) 
+        {
+             auto nodeStruct = mapStruct[s.first];
+             point_fs[s][0]=nodeStruct[0];
+             point_fs[s][1]=nodeStruct[1];
+             point_fs[s][2]=nodeStruct[2];
+        }
+
+        v
         
-        for (int j=0; j<m; ++j){
-            v[1]= point_fs[j][1] - point_fs[j+1][1];
-            v[2]= point_fs[j][2] - point_fp[j+1][2];
-            v[3]= point_fs[j][3] - point_fp[j+1][3];
-            dist_0=norme(v);
+        
+        
+        // Projeter le point_fa sur la droite et calcul de la distance
+        
+        vector <double> dist[n];
+        vector <double> v[3];
+        
+        vector <double> u(3); // vecteur directeur de la poutre
+        u[0]= point_fs[1][0] - point_fs[0][0];
+        u[1]= point_fs[1][1] - point_fs[0][1];
+        u[2]= point_fs[1][2] - point_fs[0][2];
+        
+        for (int i=0; i<=n; ++i){
+            force.point_fp.push.back(vector<double> (3));
+            double t;
+            t= (u[0](point_fa[i][0]-point_fs[0][0]) + u[1](point_fa[i][1]-point_fs[0][1]) + u[2](point_fa[i][2]-point_fs[0][2]))/pow(norme(u),2);
             
-            v[1]= point_fs[j][1] - point_fp[i][1];
-            v[2]= point_fs[j][2] - point_fp[i][2];
-            v[3]= point_fs[j][3] - point_fp[i][3];
-            dist_1=norme(v);
+            point_fp[i][0]=point_fs[0][0] + t*u[0];
+            point_fp[i][1]=point_fs[0][1] + t*u[1];
+            point_fp[i][2]=point_fs[0][2] + t*u[2];
             
-            v[1]= point_fs[j+1][1] - point_fp[i][1];
-            v[2]= point_fs[j+1][2] - point_fp[i][2];
-            v[3]= point_fs[j+1][3] - point_fp[i][3];
-            dist_2=norme(v);
+            v[0]= point_fa[i][0] - point_fp[i][0];
+            v[1]= point_fa[i][1] - point_fp[i][1];
+            v[2]= point_fa[i][2] - point_fp[i][2];
+            dist[i]=norme(v);
             
-            if dist_1<=dist_0{
-                epsilon_l= dist_2/dist_0;
-                epsilon_r= dist_1/dist_0;
+        }
+        
+        // Calcul des coefficients d'interpolation
+        ///vector <vector> forces_s[m][6];
+        double dist_0;
+        double dist_1;
+        double dist_2;
+        
+    
+        
+        for (int i=0; i<n; ++i){
+            ///auto forces = model.forces_to_inertial(i);
+            force.poids.pushback(vector<double>(3));
+            poids[i][0]=i;
+            
+            
+            for (int j=0; j<m; ++j){
+                v[0]= point_fs[j][0] - point_fs[j+1][0];
+                v[1]= point_fs[j][1] - point_fp[j+1][1];
+                v[2]= point_fs[j][2] - point_fp[j+1][2];
+                dist_0=norme(v);
                 
-                forces_s[j][1]= forces_s[j][1] + epsilon_l*forces[i][1];
-                forces_s[j][2]= forces_s[j][2] + epsilon_l*forces[i][2];
-                forces_s[j][3]= forces_s[j][3] + epsilon_l*forces[i][3];
-                forces_s[j][4]= forces_s[j][4] + epsilon_l*forces[i][4]*dist[i];
-                forces_s[j][5]= forces_s[j][5] + epsilon_l*forces[i][5]*dist[i];
-                forces_s[j][6]= forces_s[j][6] + epsilon_l*forces[i][6]*dist[i];
+                v[0]= point_fs[j][0] - point_fp[i][0];
+                v[1]= point_fs[j][1] - point_fp[i][1];
+                v[2]= point_fs[j][2] - point_fp[i][2];
+                dist_1=norme(v);
                 
-                forces_s[j+1][1]= forces_s[j+1][1] + epsilon_l*forces[i][1];
-                forces_s[j+1][2]= forces_s[j+1][2] + epsilon_l*forces[i][2];
-                forces_s[j+1][3]= forces_s[j+1][3] + epsilon_l*forces[i][3];
-                forces_s[j+1][4]= forces_s[j+1][4] + epsilon_l*forces[i][4]*dist[i];
-                forces_s[j+1][5]= forces_s[j+1][5] + epsilon_l*forces[i][5]*dist[i];
-                forces_s[j+1][6]= forces_s[j+1][6] + epsilon_l*forces[i][6]*dist[i];
+                v[0]= point_fs[j+1][0] - point_fp[i][0];
+                v[1]= point_fs[j+1][1] - point_fp[i][1];
+                v[2]= point_fs[j+1][2] - point_fp[i][2];
+                dist_2=norme(v);
                 
+                if (dist_1<=dist_0 & dist_2<=dist_0 & j != m-1){
+                    epsilon_l= dist_2/dist_0;
+                    epsilon_r= dist_1/dist_0;
+                    poids[i][0]=j;
+                    poids[i][1]=epsilon_l;
+                    poids[i][2]=epsilon_r;
+                    
+                }
+                else if (j==m-1){
+                    poids[i][0]=j;
+                    poids[i][1]=1;
+                    poids[i][2]=0;
+                    
+                }
             }
             
         }
-     
+   }
+
+   void ComputeStructureForces(interpolation_f for)
+
+   {
+        n=point_fa.size();
+        m=point_fs.size();
+        
+        vector <double> forces_s(6*m,0);
+        vector <double> M(3);
+        vector <double> r(3);
+        
+        for (int i=0; i<n; ++i){
+            auto forces = model.forces_to_inertial(i);
+            j=poids[i][0];
+            
+            forces_s[6*j]   += poids[i][1] * forces[i][0];
+            forces_s[6*j+1] += poids[i][1] * forces[i][1];
+            forces_s[6*j+2] += poids[i][1] * forces[i][2];
+            
+            forces_s[6*(j+1)]   += poids[i][2] * forces[i][0];
+            forces_s[6*(j+1)+1] += poids[i][2] * forces[i][1];
+            forces_s[6*(j+1)+2] += poids[i][2] * forces[i][2];
+            
+            M[0]=forces[i][3];
+            M[1]=forces[i][4];
+            M[2]=forces[i][5];
+            
+            r[0]=point_fa[j][0] - point_fs[i][0];
+            r[1]=point_fa[j][1] - point_fs[i][1];
+            r[2]=point_fa[j][2] - point_fs[i][2];
+            
+            M_s=crossProduct (r,M);
+            
+            forces_s[6*j+3]   += poids[i][1] * (M_s[0] + forces[i][3]);
+            forces_s[6*j+4]   += poids[i][1] * (M_s[1] + forces[i][4]);
+            forces_s[6*j+5]   += poids[i][1] * (M_s[2] + forces[i][5]);
+            
+            r[0]=point_fa[i][0] - point_fs[j+1][0];
+            r[1]=point_fa[i][1] - point_fs[j+1][1];
+            r[2]=point_fa[i][2] - point_fs[i+1][2];
+            
+            M_s=crossProduct (r,M);
+            
+            forces_s[6*(j+1)+3]   += poids[i][1] * (M_s[0] + forces[i][3]);
+            forces_s[6*(j+1)+4]   += poids[i][1] * (M_s[1] + forces[i][4]);
+            forces_s[6*(j+1)+5]   += poids[i][1] * (M_s[2] + forces[i][5]);
+            
+        }
     }
+        
       
-}
-        double dotProduct(const vector<double>& a, const vector<double>& b) {
-            if (a.size() != b.size()) {
-                throw std::runtime_error("Vectors must have the same dimension.");
-            }
-            double result = 0.0;
-            for (size_t i = 0; i < a.size(); i++) {
-                result += a[i] * b[i];
-            }
-            return result;
-         }
-         vector<double> crossProduct(const vector<double>& i, const vector<double>& j){
+
+    vector<double> crossProduct(const vector<double>& i, const vector<double>& j)
+    {
              if (i.size() !=3 || j.size() != 3){
                  throw std::runtime_error("Vectors must have dimension 3.");
              }
@@ -245,14 +305,18 @@ namespace aero{
              result[1] = i[2]*j[0] - i[0]*j[2];
              result[2] = i[0]*j[1] - i[1]*j[0];
              return result;
-         }
-         double norme(vector<double> v){
-             double sum = 0;
-             for(int i=0; i<v.size(); i++){
-                 sum += pow(v[i], 2);
-             }
-             return sqrt(sum);
-         }
+    }
+         
+         
+    double norme(vector<double> v)
+    {
+        double sum = 0;
+        for(int i=0; i<v.size(); i++)
+        {
+         sum += pow(v[i], 2);
+        }
+        return sqrt(sum);
+    }
 
 
 
