2298ca4442a7eaceb8e651811dc74f17aba659bc
amnia02 <amadou.niane@polymtl.ca>
2023-03-14 12:19:58 -0400

    Update aerotools.h

--- src/aeroelasticite/include/aero/aerotools.h
+++ src/aeroelasticite/include/aero/aerotools.h
@@ -15,4 +15,18 @@ namespace aero{
                       std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,std::vector<Vector3d> nodes,map<int,Vector3d> mapStruct,map<int, int> mapStructni);
     auto computeVLMDispalecement(interpolation pos,std::vector<surface::wingStation> wingStations,
                                  std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,std::vector<Vector3d> nodes);
+    
+    struct interpolation_f {std::vector<vector<double>> point_fa, std::vector<vector<double>> point_fs
+                      std::vector<vector<double>> poids };
+    
+    void LoadInterpol(interpolation_f &force, std::vector<surface::wingStation> wingStations,
+                       std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,map<int,Vector3d> mapStruct,map<int, int> mapStructni)
+
+    auto ComputeStructureForces(interpolation_f force);
+
+    std::vector<double> crossProduct(const std::vector<double>& i, const std::vector<double>& j);
+
+    double norme(std::vector<double> v);
+
+
 }
