f51df002efb6986f52ff422187cdf665e8b8dfcd
amnia02 <amadou.niane@polymtl.ca>
2023-03-22 20:34:51 -0400

    Update aerotools.h

--- src/aeroelasticite/include/aero/aerotools.h
+++ src/aeroelasticite/include/aero/aerotools.h
@@ -19,10 +19,10 @@ namespace aero{
     struct interpolation{std::vector<double> node;
         std::vector<double> weight;};
 
-    void DispInterpol(interpolation &pos,std::vector<surface::wingStation> wingStations,
-                      std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,std::vector<Vector3d> nodes,map<int,Vector3d> mapStruct,map<int, int> mapStructni);
-    auto computeVLMDispalecement(interpolation pos,std::vector<surface::wingStation> wingStations,
-                                 std::vector<surface::wing> wings,std::vector<element::vortexRing> vortexRings,std::vector<Vector3d> nodes);
+    void DispInterpol(interpolation &pos,std::vector<vlm::surface::wingStation> wingStations,
+                      std::vector<vlm::surface::wing> wings,std::vector<vlm::element::vortexRing> vortexRings,std::vector<Vector3d> nodes,map<int,Vector3d> mapStruct,map<int, int> mapStructni);
+    auto computeVLMDispalecement(interpolation pos,std::vector<vlm::surface::wingStation> wingStations,
+                                 std::vector<vlm::surface::wing> wings,std::vector<vlm::element::vortexRing> vortexRings,std::vector<Vector3d> nodes);
     
     struct interpolation_f {std::vector<double> point_fa; std::vector<double> point_fs;
                         std::vector<double> poids;};
