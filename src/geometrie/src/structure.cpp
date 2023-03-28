3705a307e887d588466458c8f73330bdb53489ac
SamAyala <72941105+samayala22@users.noreply.github.com>
2023-03-28 10:58:08 -0400

    fix missing header for clang

--- src/geometrie/src/structure.cpp
+++ src/geometrie/src/structure.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-#include <iomanip> 
+#include <iomanip>
+#include <tuple>
 #include "geometrie/structure.hpp"
 #include "geometrie/geometry.hpp"
 
@@ -12,9 +13,9 @@
 //Centre placé au quart de corde
 std::vector<std::vector<double>> get_center_point(std::vector<std::vector<double>> X, std::vector<std::vector<double>> Y, std::vector<std::vector<double>> Su, std::vector<std::vector<double>> Sl){
     std::vector<std::vector<double>> center(X.size(), std::vector<double>(3,0));
-    
+
     double dim = X[0].size()-1;
-    
+
     for (int i=0; i<X.size(); i++){
         center[i][0] = X[i][0] + 0.25*(X[i][dim]-X[i][0]);
         center[i][1] = Y[i][0] + 0.25*(Y[i][dim]-Y[i][0]);
@@ -22,7 +23,7 @@ std::vector<std::vector<double>> get_center_point(std::vector<std::vector<double
 
         }
     return center;
-    
+
 }
 
 std::vector<double> vecteur_normal(std::vector<double> p1, std::vector<double> p2){
@@ -34,13 +35,13 @@ std::vector<double> vecteur_normal(std::vector<double> p1, std::vector<double> p
         v1[i]=p2[i]-p1[i];
         v2[i]=p3[i]-p1[i];
     }
-    
+
     normal[0]=(v1[1]*v2[2])-(v2[1]*v1[2]);
     normal[1]=-((v1[0]*v2[2])-(v2[0]*v1[2]));
     normal[2]=-(v1[0]*v2[1])-(v2[0]*v1[1]);
-    
+
     norme=sqrt(pow(normal[0],2)+pow(normal[1],2)+pow(normal[2],2));
-    
+
     normal_unitaire[0]=normal[0]/norme;
     normal_unitaire[1]=normal[1]/norme;
     normal_unitaire[2]=normal[2]/norme;
@@ -56,9 +57,9 @@ std::vector<std::vector<std::vector<std::vector<double>>>> get_geometry(Body win
 std::vector<std::tuple<int,std::vector<double>,std::vector<double>,std::vector<double>>> maillage_structure(Body wing){
     std::vector<std::tuple<int,std::vector<double>,std::vector<double>,std::vector<double>>> element;
     std::vector<double> normal(3,0),pt_normal(3,0);
-    
+
     std::vector<std::vector<std::vector<std::vector<double>>>> surfaces = get_geometry(wing);
-    
+
     std::vector<std::vector<double>> X_U, X_L, Y_U, Y_L, SU, SL;
     for (int m=0; m < surfaces[0][0].size()-1; m++){
         X_U.push_back(surfaces[0][0][m]);
@@ -68,9 +69,9 @@ std::vector<std::tuple<int,std::vector<double>,std::vector<double>,std::vector<d
         SU.push_back(surfaces[0][4][m]);
         SL.push_back(surfaces[0][5][m]);
 
-        
+
     }
-    
+
     if (surfaces.size()>1){
         for (int k=0; k < surfaces.size()-1; k++){
             std::vector<std::vector<std::vector<double>>> surface = surfaces[k+1];
@@ -81,26 +82,26 @@ std::vector<std::tuple<int,std::vector<double>,std::vector<double>,std::vector<d
                 X_L.push_back(surface[3][m+1]);
                 SU.push_back(surface[4][m+1]);
                 SL.push_back(surface[5][m+1]);
-                
+
             }
         }
     }
-    
-    
 
-        
+
+
+
     std::vector<std::vector<double>> centre = get_center_point(X_U, Y_U, SU, SL);
-    
-    
-    
-    
-    
+
+
+
+
+
     std::ofstream myfile;
     myfile.open ("Point_maillage_structure.txt");
-    
 
 
-        
+
+
         myfile<<"$##### NOEUDS #####"<<std::endl;
         myfile<<std::setprecision(5)<<std::fixed;
         for(int i=0; i<=(centre.size()-1); i++){
@@ -115,11 +116,11 @@ std::vector<std::tuple<int,std::vector<double>,std::vector<double>,std::vector<d
             pt_normal[2]=centre[i][2]+normal[2];
             myfile<<std::setprecision(5)<<std::fixed;
             myfile<<"CBAR, "<<i+1<<", "<<"10, "<<i+100<<", "<<i+101<<", "<<normal[0]<<", "<<normal[1]<<", "<<normal[2]<<std::endl;
-            
-            element.push_back(make_tuple(i,centre[i],centre[i+1],pt_normal));
+
+            element.push_back(std::make_tuple(i,centre[i],centre[i+1],pt_normal));
 
         }
-    
+
     myfile<<""<<std::endl;
     myfile<<"$##### Propriétés de sections #####"<<std::endl;
     myfile<<"PBAR, "<<"10, "<<"11, "<<"1.0, "<<"0.1, "<<"0.1, "<<"0.1"<<std::endl;
@@ -135,7 +136,7 @@ std::vector<std::tuple<int,std::vector<double>,std::vector<double>,std::vector<d
     myfile<<"ENDDATA "<<std::endl;
 
     myfile.close();
-    
+
     return element;
 }
 
