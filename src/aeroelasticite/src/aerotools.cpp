b9d4c0fb6e0a4b21f85e44bae16f2d885739fc56
ednacob <114762770+ednacob@users.noreply.github.com>
2023-03-30 00:07:24 -0400

    Update aerotools.cpp

--- src/aeroelasticite/src/aerotools.cpp
+++ src/aeroelasticite/src/aerotools.cpp
@@ -30,9 +30,9 @@ namespace aero{
     void DispInterpol( interpolation &pos,std::vector<vlm::surface::wingStation> wingStations,
                        std::vector<vlm::surface::wing> wings,std::vector<element::vortexRing> vortexRings,
                       std::vector<Vector3d> nodes,std::map<int, Vector3d> mapStruct,std::map<int, int> mapStructni) {
-        for (int k = 0; k <wings.size() ; ++k)
+        for (int k = 0; k <wingStations.size() ; ++k)
         {
-            for (int i = 0; i <wingStations.size() ; ++i)
+            for (int i = 0; i <vortexRings.size() ; ++i)
             {
 
                 auto wstation= wingStations[wings[0].get_stationIDs()[k]];
@@ -41,7 +41,7 @@ namespace aero{
 
 
 
-                for (int p=0; p<vortexRings.size(); ++p)
+                for (int p=0; p<vring.size(); ++p)
                 {
                     auto node=nodes[vring.get_nodeIDs()[p]];
                     double bestDistance[2]={10000,10000};
@@ -99,13 +99,13 @@ namespace aero{
 
     auto computeVLMDispalecement(interpolation pos,std::vector<vlm::surface::wingStation> wingStations,
                                  std::vector<vlm::surface::wing> wings,std::vector<vlm::element::vortexRing> vortexRings,std::vector<Vector3d> nodes,std::vector<Eigen::VectorXd> Solutions) {
-        for (int k = 0; k<wings.size(); ++k) {
-            for (int i = 0; i<wingStations.size(); ++i) {
+        for (int k = 0; k<wingStations.size(); ++k) {
+            for (int i = 0; i<vortexRings.size(); ++i) {
 
 
                 auto wstation= wingStations[wings[0].get_stationIDs()[k]];
                 auto vring = vortexRings[wstation.get_vortexIDs()[i]];
-                int numPointsVort = vortexRings.size();
+                int numPointsVort = vring.size();
                 double angle_x;
                 double angle_y;
                 double angle_z;
